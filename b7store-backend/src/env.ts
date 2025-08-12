eb63af5c59b578aaea1c69e85a87223e0fa439d1
Diego Viana <dsantos@inf.ufsm.br>
2025-08-12 18:19:41 -0300

    Update env.ts

--- b7store-backend/src/env.ts
+++ b7store-backend/src/env.ts
@@ -5,6 +5,9 @@ const envSchema = z.object({
   DATABASE_URL: z.string(),
   JWT_SECRET_KEY: z.string(),
   BASE_URL: z.string(),
+  STRIPE_SECRET_KEY: z.string(),
+  FRONTEND_URL: z.string(),
+  STRIPE_WEBHOOK_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
