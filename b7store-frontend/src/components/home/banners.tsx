badf1e8f319b4e8f6d09fa911e63b72c3c5b7fa9
Diego Viana <dsantos@inf.ufsm.br>
2025-08-13 16:08:56 -0300

    banner component completion

--- b7store-frontend/src/components/home/banners.tsx
+++ b7store-frontend/src/components/home/banners.tsx
@@ -1,12 +1,36 @@
+'use client'
+
 import { Banner } from '@/types/banner'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useCallback, useEffect, useState } from 'react'
 
 type BannersProps = {
   list: Banner[]
 }
 
+let bannerTimer: NodeJS.Timeout
+
 export function Banners({ list }: BannersProps) {
+  const [currentImage, setCurrentImage] = useState(0)
+
+  const nextImage = useCallback(() => {
+    setCurrentImage((current) => (current + 1 >= list.length ? 0 : current + 1))
+  }, [list.length])
+
+  const handleBannerClick = (index: number) => {
+    setCurrentImage(index)
+
+    clearInterval(bannerTimer)
+    bannerTimer = setInterval(nextImage, 5000)
+  }
+
+  useEffect(() => {
+    bannerTimer = setInterval(nextImage, 5000)
+
+    return () => clearInterval(bannerTimer)
+  }, [nextImage])
+
   return (
     <div>
       <div className="relative aspect-[3/1]">
@@ -14,7 +38,8 @@ export function Banners({ list }: BannersProps) {
           <Link
             key={index}
             href={banner.link}
-            className="absolute inset-0 duration-300"
+            className={`absolute inset-0 duration-1000`}
+            style={{ opacity: currentImage === index ? 1 : 0 }}
           >
             <Image
               src={banner.img}
@@ -26,7 +51,16 @@ export function Banners({ list }: BannersProps) {
           </Link>
         ))}
       </div>
-      <div>.... botoes</div>
+      <div className="mt-4 flex justify-center gap-4">
+        {list.map((banner, index) => (
+          <div
+            key={index}
+            className="size-4 cursor-pointer rounded-full bg-blue-600 duration-1000"
+            style={{ opacity: currentImage === index ? 1 : 0.3 }}
+            onClick={() => handleBannerClick(index)}
+          />
+        ))}
+      </div>
     </div>
   )
 }
