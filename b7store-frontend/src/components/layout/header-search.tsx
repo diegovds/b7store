72a1cc32208811f9dbb225338b97e612b15c074d
Diego Viana <dsantos@inf.ufsm.br>
2025-08-13 02:28:52 -0300

    Update header-search.tsx

--- b7store-frontend/src/components/layout/header-search.tsx
+++ b7store-frontend/src/components/layout/header-search.tsx
@@ -2,7 +2,7 @@ export function HeaderSearch() {
   return (
     <input
       type="search"
-      className="w-full rounded-sm border border-gray-200 bg-[url(/assets/ui/search.png)] bg-[size:24px] bg-[12px_50%] bg-no-repeat py-3 pl-12 text-base outline-none"
+      className="w-full rounded-sm border border-gray-200 bg-[url(/assets/ui/search.png)] bg-[size:24px] bg-[12px_50%] bg-no-repeat py-3 pr-4 pl-12 text-base outline-none"
       placeholder="O que você procura?"
     />
   )
