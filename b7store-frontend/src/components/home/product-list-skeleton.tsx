732e54b4e29a9f455cab3593e1a8ff601d5240c4
Diego Viana <dsantos@inf.ufsm.br>
2025-08-13 17:25:28 -0300

    add ProductListSkeleton component

---
+++ b7store-frontend/src/components/home/product-list-skeleton.tsx
@@ -0,0 +1,17 @@
+export function ProductListSkeleton() {
+  return (
+    <div className="mt-12 md:mt-[69px]">
+      <div className="flex flex-col items-center gap-4 md:items-start">
+        <div className="h-8 w-52 animate-pulse rounded bg-gray-200" />
+        <div className="h-5 w-64 animate-pulse rounded bg-gray-200" />
+      </div>
+
+      <div className="mt-8 grid grid-cols-1 gap-8 md:mt-10 md:grid-cols-4">
+        <div className="h-96 animate-pulse rounded bg-gray-200" />
+        <div className="h-96 animate-pulse rounded bg-gray-200" />
+        <div className="h-96 animate-pulse rounded bg-gray-200" />
+        <div className="h-96 animate-pulse rounded bg-gray-200" />
+      </div>
+    </div>
+  )
+}
