e0a82b29b1e21fe83b6854417553735f5051a05a
Diego Viana <dsantos@inf.ufsm.br>
2025-08-13 16:37:50 -0300

    add InformationIcon component in homepage

--- b7store-frontend/src/app/(site)/page.tsx
+++ b7store-frontend/src/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import { Banners } from '@/components/home/banners'
+import { InformationIcon } from '@/components/home/information-icon'
 import { getBanners } from '@/http/api'
 
 export default async function Home() {
@@ -7,6 +8,23 @@ export default async function Home() {
   return (
     <div>
       <Banners list={data.banners} />
+      <div className="mt-6 flex flex-col gap-4 md:mt-12 md:flex-row md:gap-8">
+        <InformationIcon
+          icon="/assets/ui/truck-line.png"
+          title="Frete Grátis"
+          description="Para todo o Nordeste."
+        />
+        <InformationIcon
+          icon="/assets/ui/discount-percent-line.png"
+          title="Muitas ofertas"
+          description="Ofertas imbátiveis."
+        />
+        <InformationIcon
+          icon="/assets/ui/arrow-left-right-line.png"
+          title="Troca fácil"
+          description="No período de 30 dias."
+        />
+      </div>
     </div>
   )
 }
