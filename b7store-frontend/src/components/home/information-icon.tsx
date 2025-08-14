28e2b45ffc95630148043c2daaca36d21981586b
Diego Viana <dsantos@inf.ufsm.br>
2025-08-13 21:09:59 -0300

    Update information-icon.tsx

--- b7store-frontend/src/components/home/information-icon.tsx
+++ b7store-frontend/src/components/home/information-icon.tsx
@@ -17,7 +17,7 @@ export function InformationIcon({
         <Image src={icon} alt={title} width={40} height={40} />
       </div>
       <div className="flex-1 pl-8">
-        <h3 className="text-xl font-bold">{title}</h3>
+        <h3 className="text-xl font-medium">{title}</h3>
         <p className="text-sm text-gray-500">{description}</p>
       </div>
     </div>
