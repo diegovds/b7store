5c4505a7294011b17abc4418c542e4b9379342c3
Diego Viana <dsantos@inf.ufsm.br>
2025-08-30 13:35:12 -0300

    create auth store

--- b7store-frontend/src/libs/server-cookies.ts
+++ b7store-frontend/src/libs/server-cookies.ts
@@ -1,6 +1,23 @@
 import { CartItem } from '@/types/cart-item'
 import { cookies } from 'next/headers'
 
+// auth cookie
+export const getServerAuthToken = async (): Promise<string | null> => {
+  const cookiesStore = await cookies()
+  return cookiesStore.get('auth_token')?.value ?? null
+}
+
+export const setServerAuthToken = async (token: string) => {
+  const cookiesStore = await cookies()
+  cookiesStore.set('auth_token', token, { httpOnly: true })
+}
+
+export const clearServerAuthToken = async () => {
+  const cookiesStore = await cookies()
+  cookiesStore.delete('auth_token')
+}
+
+// cart cookie
 export async function getServerCart(): Promise<CartItem[]> {
   const cookiesStore = await cookies()
   const value = cookiesStore.get('cart')?.value
--- b7store-frontend/src/providers/store-hydration.tsx
+++ b7store-frontend/src/providers/store-hydration.tsx
@@ -1,17 +1,26 @@
 'use client'
 
+import { getAuthState } from '@/actions/get-auth-state'
 import { getCartState } from '@/actions/get-cart-state'
+import { useAuthStore } from '@/store/auth'
 import { useCartStore } from '@/store/cart'
 import { useEffect } from 'react'
 
 export const StoreHydration = () => {
+  const { setHydrated, setToken } = useAuthStore()
+
   useEffect(() => {
+    getAuthState().then(({ token }) => {
+      if (token) setToken(token)
+      setHydrated(true)
+    })
+
     getCartState().then(({ cart }) => {
       if (cart.length > 0) {
         useCartStore.setState({ cart })
       }
     })
-  }, [])
+  }, [setHydrated, setToken])
 
   return null
 }
