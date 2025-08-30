7a00311e057336ababc33429ae313b26d766d66e
Diego Viana <dsantos@inf.ufsm.br>
2025-08-30 12:56:48 -0300

    Create FinishPurchaseButton.tsx

---
+++ b7store-frontend/src/components/cart/FinishPurchaseButton.tsx
@@ -0,0 +1,7 @@
+export const FinishPurchaseButton = () => {
+  return (
+    <button className="w-full cursor-pointer rounded-sm border-0 bg-blue-600 px-6 py-5 text-white">
+      Finalizar compra
+    </button>
+  )
+}
