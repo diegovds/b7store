395e208935759986b9968179a57d0df5011f8881
Diego Viana <dsantos@inf.ufsm.br>
2025-08-30 02:32:24 -0300

    Create cart-product-item.tsx

---
+++ b7store-frontend/src/components/cart/cart-product-item.tsx
@@ -0,0 +1,98 @@
+import { setCartState } from '@/actions/set-cart-state'
+import { useCartStore } from '@/store/cart'
+import { CartListItem } from '@/types/cart-list-item'
+import Image from 'next/image'
+
+type CartProductItemProps = {
+  item: CartListItem
+}
+
+export const CartProductItem = ({ item }: CartProductItemProps) => {
+  const cartStore = useCartStore((state) => state)
+
+  const updateCookie = async () => {
+    const updatedCart = useCartStore.getState().cart
+    await setCartState(updatedCart)
+  }
+
+  const handleMinus = async () => {
+    if (item.quantity > 1) {
+      cartStore.updateQuantity(item.product.id, item.quantity - 1)
+      await updateCookie()
+    } else {
+      await handleRemove()
+    }
+  }
+
+  const handlePlus = async () => {
+    cartStore.updateQuantity(item.product.id, item.quantity + 1)
+    await updateCookie()
+  }
+
+  const handleRemove = async () => {
+    cartStore.removeItem(item.product.id)
+    await updateCookie()
+  }
+
+  return (
+    <div className="flex gap-4 border-0 border-gray-200 p-6 md:gap-8 md:border-b">
+      <div className="border border-gray-200 p-1">
+        {item.product.image && (
+          <Image
+            src={item.product.image}
+            alt={item.product.label}
+            width={96}
+            height={96}
+            className="size-24 md:size-20"
+          />
+        )}
+      </div>
+      <div className="flex flex-1 flex-col justify-between md:flex-row md:items-center">
+        <div className="space-y-0 md:space-y-1.5">
+          <div className="text-sm font-medium">{item.product.label}</div>
+          <div className="hidden text-xs text-gray-500 md:block">
+            CÓD: {item.product.id}
+          </div>
+        </div>
+        <div>
+          <div className="flex w-fit rounded-sm border border-gray-200 text-lg font-medium text-[#7f7f7f]">
+            <button
+              onClick={handleMinus}
+              className="flex size-10 cursor-pointer items-center justify-center"
+            >
+              -
+            </button>
+            <div className="flex size-10 items-center justify-center border-x border-gray-200">
+              {item.quantity}
+            </div>
+            <button
+              onClick={handlePlus}
+              className="flex size-10 cursor-pointer items-center justify-center"
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-24 flex-col items-end justify-between md:w-40 md:flex-row md:items-center">
+        <div className="text-lg font-medium text-blue-600">
+          R$ {item.product.price.toFixed(2)}
+        </div>
+        <div>
+          <button
+            onClick={handleRemove}
+            className="flex size-10 cursor-pointer items-center justify-center rounded border border-gray-200 md:size-12"
+          >
+            <Image
+              src={'/assets/ui/trash.png'}
+              alt=""
+              width={24}
+              height={24}
+              className="size-5 md:size-6"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
