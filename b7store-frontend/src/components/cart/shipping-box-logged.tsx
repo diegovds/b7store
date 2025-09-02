492fe1df3f0249d2c722a04d99943f6a7e96367c
Diego Viana <dsantos@inf.ufsm.br>
2025-09-02 13:07:27 -0300

    Update shipping-box-logged.tsx

--- b7store-frontend/src/components/cart/shipping-box-logged.tsx
+++ b7store-frontend/src/components/cart/shipping-box-logged.tsx
@@ -1,14 +1,32 @@
 'use client'
 
+import { getShippingInfo } from '@/actions/get-shipping-info'
 import { getUserAddresses, GetUserAddresses200AddressItem } from '@/http/api'
 import { useAuthStore } from '@/store/auth'
+import { useCartStore } from '@/store/cart'
 import { useEffect, useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
 
 export const ShippingBoxLogged = () => {
-  const { token } = useAuthStore()
+  const { token, hydrated } = useAuthStore()
+  const {
+    shippingZipcode,
+    clearShipping,
+    selectedAdressId,
+    setShippingCost,
+    setShippingDays,
+    setShippingZipcode,
+    setSelectedAddressId,
+  } = useCartStore()
   const [addresses, setAddresses] = useState<
-    GetUserAddresses200AddressItem[] | null
-  >(null)
+    GetUserAddresses200AddressItem[] | []
+  >([])
 
   useEffect(() => {
     const fetchAddresses = async () => {
@@ -20,12 +38,53 @@ export const ShippingBoxLogged = () => {
       }
     }
 
-    if (token) {
+    if (token && hydrated) {
       fetchAddresses()
     }
-  }, [token])
+  }, [token, hydrated])
 
-  console.log(addresses)
+  useEffect(() => {
+    const updateShippingInfo = async () => {
+      if (shippingZipcode.length > 4) {
+        const shippingInfo = await getShippingInfo(shippingZipcode)
+        if (shippingInfo) {
+          setShippingCost(shippingInfo.cost)
+          setShippingDays(shippingInfo.days)
+        }
+      }
+    }
 
-  return <div>...</div>
+    if (selectedAdressId) {
+      updateShippingInfo()
+    }
+  }, [selectedAdressId, setShippingCost, setShippingDays, shippingZipcode])
+
+  const handleSelectAddress = (value: string) => {
+    clearShipping()
+    const address = addresses.find((a) => a.id.toString() === value)
+    if (address) {
+      setShippingZipcode(address.zipcode)
+      setSelectedAddressId(parseInt(value))
+    }
+  }
+
+  return (
+    <div className="flex gap-4">
+      <Select onValueChange={handleSelectAddress}>
+        <SelectTrigger className="h-full flex-1 px-6 py-8">
+          <SelectValue placeholder="Selecione um endereço" />
+        </SelectTrigger>
+        <SelectContent className="rounded-sm border border-gray-200 bg-white">
+          {addresses.map((address) => (
+            <SelectItem key={address.id} value={address.id.toString()}>
+              {`${address.street}, ${address.number} - ${address.city} (${address.zipcode})`}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <button className="cursor-pointer rounded-sm border-0 bg-blue-600 px-6 py-5 text-white">
+        +
+      </button>
+    </div>
+  )
 }
