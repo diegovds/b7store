dddaf5195074490eb9e785bfa7c94c157a093f00
Diego Viana <dsantos@inf.ufsm.br>
2025-09-03 20:22:52 -0300

    Update my-addresses.tsx

--- b7store-frontend/src/components/my-orders/my-addresses.tsx
+++ b7store-frontend/src/components/my-orders/my-addresses.tsx
@@ -1,22 +1,58 @@
 'use client'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { GetUserAddresses200AddressItem } from '@/http/api'
+import { getUserAddresses, GetUserAddresses200AddressItem } from '@/http/api'
+import { useAuthStore } from '@/store/auth'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { AddressModal } from '../cart/address-modal'
 
 type MyAddressesProps = {
   addresses: GetUserAddresses200AddressItem[]
 }
 
+type UseAddressesProps = {
+  token: string | null
+  hydrated: boolean
+}
+
 export const MyAddresses = ({ addresses }: MyAddressesProps) => {
+  const [modalOpened, setModalOpened] = useState(false)
+  const { token, hydrated } = useAuthStore()
+
+  function useAddresses({ token, hydrated }: UseAddressesProps) {
+    return useQuery({
+      queryKey: ['user-addresses', token],
+      queryFn: async () => {
+        if (!token) return []
+        const response = await getUserAddresses({
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        return response.address ?? []
+      },
+      enabled: !!token && hydrated,
+    })
+  }
+
+  const { data: _addresses = addresses } = useAddresses({ token, hydrated })
+
   return (
     <div className="space-y-4">
-      <h2 className="text-2xl font-bold text-gray-800">
-        Você possui {addresses.length}{' '}
-        {addresses.length !== 1 ? 'endereços' : 'endereço'}:
-      </h2>
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold text-gray-800">
+          Você possui {_addresses.length}{' '}
+          {_addresses.length !== 1 ? 'endereços' : 'endereço'}:
+        </h2>
+        <button
+          onClick={() => setModalOpened(true)}
+          className="cursor-pointer rounded-sm border-0 bg-blue-600 px-4 py-2 text-white duration-300 hover:bg-blue-700"
+        >
+          Adicionar endereço
+        </button>
+      </div>
 
       <div className="grid gap-4 md:grid-cols-2">
-        {addresses.map((address) => (
+        {_addresses.map((address) => (
           <Card key={address.id} className="border border-gray-200 shadow-none">
             <CardHeader>
               <CardTitle className="text-lg font-semibold">
@@ -44,6 +80,7 @@ export const MyAddresses = ({ addresses }: MyAddressesProps) => {
           </Card>
         ))}
       </div>
+      <AddressModal open={modalOpened} onClose={() => setModalOpened(false)} />
     </div>
   )
 }
