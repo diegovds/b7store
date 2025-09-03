de09cb929f000278bf2dc3699e2cd41869bf1469
Diego Viana <dsantos@inf.ufsm.br>
2025-09-02 22:45:45 -0300

    Update my-addresses.tsx

--- b7store-frontend/src/components/my-orders/my-addresses.tsx
+++ b7store-frontend/src/components/my-orders/my-addresses.tsx
@@ -1,5 +1,11 @@
 'use client'
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import { GetUserAddresses200AddressItem } from '@/http/api'
 
 type MyAddressesProps = {
@@ -7,5 +13,45 @@ type MyAddressesProps = {
 }
 
 export const MyAddresses = ({ addresses }: MyAddressesProps) => {
-  return <div>meus endereços</div>
+  return (
+    <div className="space-y-4">
+      <h2 className="text-2xl font-bold text-gray-800">
+        Você possui {addresses.length}{' '}
+        {addresses.length !== 1 ? 'endereços' : 'endereço'}
+      </h2>
+
+      <Accordion type="single" collapsible className="w-full">
+        {addresses.map((address) => (
+          <AccordionItem key={address.id} value={address.id.toString()}>
+            <AccordionTrigger>
+              {address.street}, {address.number}
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="space-y-1 text-sm text-gray-700">
+                <p>
+                  <span className="font-medium">Rua:</span> {address.street}
+                </p>
+                <p>
+                  <span className="font-medium">Número:</span> {address.number}
+                </p>
+                {address.complement && (
+                  <p>
+                    <span className="font-medium">Complemento:</span>{' '}
+                    {address.complement}
+                  </p>
+                )}
+                <p>
+                  <span className="font-medium">Cidade:</span> {address.city} -{' '}
+                  {address.state}
+                </p>
+                <p>
+                  <span className="font-medium">CEP:</span> {address.zipcode}
+                </p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  )
 }
