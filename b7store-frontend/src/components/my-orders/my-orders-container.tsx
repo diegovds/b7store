dce021ac31c97ad12f7ee9b1531966c251e88fde
Diego Viana <dsantos@inf.ufsm.br>
2025-09-03 13:16:30 -0300

    adding the logout button to the my orders page

--- b7store-frontend/src/components/my-orders/my-orders-container.tsx
+++ b7store-frontend/src/components/my-orders/my-orders-container.tsx
@@ -1,6 +1,10 @@
 'use client'
 
+import { clearAuthCookie } from '@/actions/clear-auth-cookie'
 import { GetOrdersId200Order, GetUserAddresses200AddressItem } from '@/http/api'
+import { useAuthStore } from '@/store/auth'
+import { LogOut } from 'lucide-react'
+import { redirect } from 'next/navigation'
 import { useState } from 'react'
 import { MyAddresses } from './my-addresses'
 import { MyOrders } from './my-orders'
@@ -15,24 +19,37 @@ export const MyOrdersContainer = ({
   myOrders,
 }: MyOrdersContainerProsp) => {
   const [viewOrders, setViewOrders] = useState(true)
+  const { clearToken } = useAuthStore()
 
   return (
     <div className="min-h-screen">
       <div className="flex flex-col">
-        <div className="flex gap-2 py-4">
+        <div className="flex items-center justify-between gap-2 py-4">
+          <div className="flex gap-2">
+            <button
+              disabled={viewOrders}
+              onClick={() => setViewOrders(true)}
+              className={`cursor-pointer border-0 border-b pb-1 text-black duration-300 disabled:cursor-default ${viewOrders ? 'border-gray-300 font-medium opacity-65' : 'border-transparent font-normal'}`}
+            >
+              Meus Pedidos
+            </button>
+            <button
+              disabled={!viewOrders}
+              onClick={() => setViewOrders(false)}
+              className={`cursor-pointer border-0 border-b pb-1 text-black duration-300 disabled:cursor-default ${!viewOrders ? 'border-gray-300 font-medium opacity-65' : 'border-transparent font-normal'}`}
+            >
+              Meus Endereços
+            </button>
+          </div>
           <button
-            disabled={viewOrders}
-            onClick={() => setViewOrders(true)}
-            className={`cursor-pointer border-0 border-b pb-1 text-black duration-300 disabled:cursor-default ${viewOrders ? 'border-gray-300 opacity-65' : 'border-transparent'}`}
+            className="cursor-pointer"
+            onClick={async () => {
+              await clearAuthCookie()
+              clearToken()
+              redirect('/')
+            }}
           >
-            Meus Pedidos
-          </button>
-          <button
-            disabled={!viewOrders}
-            onClick={() => setViewOrders(false)}
-            className={`cursor-pointer border-0 border-b pb-1 text-black duration-300 disabled:cursor-default ${!viewOrders ? 'border-gray-300 opacity-65' : 'border-transparent'}`}
-          >
-            Meus Endereços
+            <LogOut />
           </button>
         </div>
         <div>
