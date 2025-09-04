4e3b2aac66319e66938355966587be8aa6f7cf8a
Diego Viana <dsantos@inf.ufsm.br>
2025-09-04 00:20:59 -0300

    Update product-description.tsx

--- b7store-frontend/src/components/product/product-description.tsx
+++ b7store-frontend/src/components/product/product-description.tsx
@@ -1,41 +1,32 @@
-'use client'
-
-import Image from 'next/image'
-import { useState } from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../ui/accordion'
 
 type ProductDescriptionProps = {
   text: string | null
 }
 
 export function ProductDescription({ text }: ProductDescriptionProps) {
-  const [opened, setOpened] = useState(true)
+  if (text === null) return null
 
   return (
-    <div className="mt-20 border border-gray-200 bg-white p-7">
-      <div
-        className={`flex items-center justify-between ${opened ? 'border-b pb-7' : 'border-b-0'} border-b-gray-200`}
-      >
-        <div className="text-base font-medium md:text-2xl">
+    <Accordion
+      className="mt-20 border border-gray-200 bg-white p-7"
+      type="single"
+      collapsible
+    >
+      <AccordionItem value={text}>
+        <AccordionTrigger className="text-base font-medium hover:no-underline md:text-2xl">
           Informações do produto
-        </div>
-        <div
-          onClick={() => setOpened(!opened)}
-          className="flex size-14 cursor-pointer items-center justify-center rounded-sm border border-gray-200"
-        >
-          <Image
-            src={`/assets/ui/arrow-left-s-line.png`}
-            alt=""
-            width={24}
-            height={24}
-            className={`duration-300 ${opened ? 'rotate-0' : 'rotate-180'}`}
-          />
-        </div>
-      </div>
-      <div
-        className={`overflow-y-hidden text-sm text-gray-500 duration-300 md:text-base ${opened ? 'mt-7 max-h-screen' : 'max-h-0'}`}
-      >
-        {text}
-      </div>
-    </div>
+        </AccordionTrigger>
+
+        <AccordionContent className="mt-7 border-t border-t-gray-200 pt-7 text-sm text-gray-500 md:text-base">
+          {text}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
