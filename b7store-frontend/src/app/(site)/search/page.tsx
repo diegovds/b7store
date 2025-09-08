882fb1f598622787ddb0567c7851d327b51261d8
Diego Viana <dsantos@inf.ufsm.br>
2025-09-08 20:00:40 -0300

    update SearchPage

--- b7store-frontend/src/app/(site)/search/page.tsx
+++ b7store-frontend/src/app/(site)/search/page.tsx
@@ -1,9 +1,58 @@
+import { getProducts } from '@/http/api'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
+
 type SearchPageProps = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-export default async function SearchPage({ searchParams }: SearchPageProps) {
+export async function generateMetadata({
+  searchParams,
+}: SearchPageProps): Promise<Metadata> {
   const search = await searchParams
 
-  return <div>Busca por {search.q}:</div>
+  if (!search.q) {
+    redirect('/')
+  }
+
+  return {
+    title: `Busca por ${search.q}`,
+  }
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const params = await searchParams
+
+  const q = typeof params.q === 'string' ? params.q : undefined
+
+  const order =
+    params.order === 'views'
+      ? 'views'
+      : params.order === 'price'
+        ? 'price'
+        : params.order === 'selling'
+          ? 'selling'
+          : undefined
+
+  const { q: _q, order: _order, ...filters } = params
+
+  const { categoryIds, error, products } = await getProducts({
+    orderBy: order,
+    limit: '8',
+    metadata: JSON.stringify(filters),
+    q,
+  })
+
+  if (error) {
+    redirect('/')
+  }
+
+  return (
+    <div>
+      <div className="my-4 text-base text-gray-500">
+        <Link href="/">Home</Link> &gt; Busca por {q}
+      </div>
+    </div>
+  )
 }
